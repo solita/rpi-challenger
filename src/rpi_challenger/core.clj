c7101189da24c94777ad7354bb7524dd80646c74
Esko Luontola <esko.luontola@solita.fi>
2012-11-11 21:49:39 +0200

    Use Enlive templates

--- src/rpi_challenger/core.clj
+++ src/rpi_challenger/core.clj
@@ -1,16 +1,20 @@
 (ns rpi-challenger.core
-  (:require [net.cgrand.enlive-html :as html])
   (:use ring.util.response
+        net.cgrand.enlive-html
         [net.cgrand.moustache :only [app]]))
 
-(def layout (html/html-resource "resources/layout.html"))
+(deftemplate layout "resources/layout.html" [body]
+  [:#content ] (content body))
+
+(defn html-page [template & args]
+  (->
+    (response (apply str (apply template args)))
+    (content-type "text/html")
+    (charset "UTF-8")))
 
 (def routes
   (app
-    [""] (fn [req] (->
-                     (response "the index page")
-                     (content-type "text/html")
-                     (charset "UTF-8")))
+    [""] (fn [req] (html-page layout "Hello world!"))
     [&] (fn [req] (->
                     (not-found "Page Not Found")
                     (content-type "text/html")
