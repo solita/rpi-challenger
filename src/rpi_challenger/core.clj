d623e87e680e23e25407f1ab16a26f443ac2fd90
Esko Luontola <esko.luontola@solita.fi>
2012-11-13 22:03:14 +0200

    Stub handler for the registration form

--- src/rpi_challenger/core.clj
+++ src/rpi_challenger/core.clj
@@ -1,8 +1,13 @@
 (ns rpi-challenger.core
   (:use ring.util.response
-        compojure.core)
+        compojure.core
+        [ring.middleware.params :only [wrap-params]])
   (:require [compojure.route :as route]
             [net.cgrand.enlive-html :as html]))
+
+(defn using-template
+  [template & args]
+  (response (apply str (apply template args))))
 
 (html/deftemplate layout "public/layout.html"
   [{:keys [title main]}]
@@ -12,12 +17,17 @@
 (html/defsnippet overview "public/overview.html" [:body ]
   [{:keys []}])
 
-(defn using-template [template & args]
-  (response (apply str (apply template args))))
+(defn handle-register-form
+  [{name "name" url "url"}]
+  (println "Registering" name "with url" url) ; TODO
+  (redirect "/"))
 
-(defroutes app
+(defroutes app-routes
   (GET "/" [] (using-template layout {:title "Overview"
                                       :main (overview {})}))
-  (POST "/register" [] "TODO: handle registration form")
+  (POST "/register" {params :params} (handle-register-form params))
   (route/resources "/")
   (route/not-found "Page Not Found"))
+
+(def app (-> app-routes
+           (wrap-params)))
