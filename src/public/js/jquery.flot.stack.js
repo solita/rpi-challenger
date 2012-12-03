f2eeebe0fb6067e2b8dce295c279e4644cd0f165
Esko Luontola <esko.luontola@solita.fi>
2012-12-03 22:37:55 +0200

    Flot, latest version (v0.7-197-g8df1147)

--- src/public/js/jquery.flot.stack.js
+++ src/public/js/jquery.flot.stack.js
@@ -38,7 +38,7 @@ adjusted (e.g for bar charts or filled areas).
     
     function init(plot) {
         function findMatchingSeries(s, allseries) {
-            var res = null
+            var res = null;
             for (var i = 0; i < allseries.length; ++i) {
                 if (s == allseries[i])
                     break;
@@ -71,7 +71,7 @@ adjusted (e.g for bar charts or filled areas).
                 fromgap = true,
                 keyOffset = horizontal ? 1 : 0,
                 accumulateOffset = horizontal ? 0 : 1,
-                i = 0, j = 0, l;
+                i = 0, j = 0, l, m;
 
             while (true) {
                 if (i >= points.length)
