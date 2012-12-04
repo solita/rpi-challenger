7544723d47a35ca2634d34f379b82d57e4498b34
Esko Luontola <esko.luontola@solita.fi>
2012-12-04 14:18:38 +0200

    Code reformat

--- src/public/js/overall-score-stats.js
+++ src/public/js/overall-score-stats.js
@@ -1,51 +1,52 @@
-$(function() {
-	
-	var graphElem = $('#current-overall-scores');
-	
-	if (graphElem[0]) {
-		var url = graphElem.attr('data-url');
-				
-		var drawBarGraph = function(data) {
-			var mapDataSeries = function(data) {
-				return data.map(function(x, index) {
-					return {
-						label: x.name,
-						data: [[index, x.score]]
-					}
-				});
-			};
-			
-			var mapDataNames = function(data) {
-				return data.map(function(x, index) {
-					return [index, x.name];
-				});
-			};
-			
-			$.plot(graphElem, mapDataSeries(data), {
-				series: {
-					lines: {show: false, steps: false},
-					bars: {show: true, barWidth: 0.9, align: 'center'}
-				},
-				xaxis: {
-					ticks: mapDataNames(data)
-				}
-			});
-		};
-		
-		var lastData = {};
-		
-		var updateStats = function() {
-			$.getJSON(url, function(data) {
-				lastData = data;
-				drawBarGraph(lastData);
-			});
-		};
-		
-		setInterval(updateStats, 5000);
-		updateStats();
-		
-		$(window).resize(function() {
-			drawBarGraph(lastData);
-		});
-	}
+$(function () {
+    var graphElem = $('#current-overall-scores');
+
+    if (graphElem[0]) {
+        var url = graphElem.attr('data-url');
+
+        var drawBarGraph = function (data) {
+            var mapDataSeries = function (data) {
+                return data.map(function (x, index) {
+                    return {
+                        label:x.name,
+                        data:[
+                            [index, x.score]
+                        ]
+                    }
+                });
+            };
+
+            var mapDataNames = function (data) {
+                return data.map(function (x, index) {
+                    return [index, x.name];
+                });
+            };
+
+            $.plot(graphElem, mapDataSeries(data), {
+                series:{
+                    lines:{show:false, steps:false},
+                    bars:{show:true, barWidth:0.9, align:'center'}
+                },
+                xaxis:{
+                    ticks:mapDataNames(data)
+                }
+            });
+        };
+
+        var lastData = {};
+
+        var updateStats = function () {
+            $.getJSON(url, function (data) {
+                lastData = data;
+                drawBarGraph(lastData);
+            });
+        };
+
+        setInterval(updateStats, 5000);
+        updateStats();
+
+        $(window).resize(function () {
+            drawBarGraph(lastData);
+        });
+    }
 });
