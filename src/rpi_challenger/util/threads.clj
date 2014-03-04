14a9c0ae9f74351163e2ce1ca13720254631b1d6
Esko Luontola <esko.luontola@gmail.com>
2014-03-04 19:00:39 +0200

    Log failures in worker threads; before the executor swallowed them

--- src/rpi_challenger/util/threads.clj
+++ src/rpi_challenger/util/threads.clj
@@ -1,6 +1,9 @@
 (ns rpi-challenger.util.threads
   (:import [com.google.common.util.concurrent ThreadFactoryBuilder]
-           [java.util.concurrent ExecutorService ScheduledExecutorService TimeUnit]))
+           [java.util.concurrent ExecutorService ScheduledExecutorService TimeUnit]
+           [org.slf4j LoggerFactory]))
+
+(def ^:dynamic logger (LoggerFactory/getLogger (str (ns-name *ns*))))
 
 (defn daemon-thread-factory [name-prefix]
   (-> (ThreadFactoryBuilder.)
@@ -8,8 +11,14 @@
       (.setDaemon true)
       (.build)))
 
+(defn with-error-logging [f]
+  #(try
+    (f)
+    (catch Exception e
+      (.error logger "Uncaught exception" e))))
+
 (defn ^:dynamic schedule-repeatedly [^ScheduledExecutorService scheduler f interval-in-seconds]
-  (.scheduleAtFixedRate scheduler f 0 interval-in-seconds TimeUnit/SECONDS))
+  (.scheduleAtFixedRate scheduler (with-error-logging f) 0 interval-in-seconds TimeUnit/SECONDS))
 
 (defn ^:dynamic submit [^ExecutorService executor f]
-  (.submit executor f))
+  (.submit executor (with-error-logging f)))
